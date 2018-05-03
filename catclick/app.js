d42663c1ac7e70bac7eda3f6fe4be9aa18574c11
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-05-03 10:16:11 +0800

    temp

---
+++ catclick/app.js
@@ -0,0 +1,48 @@
+let catPhoto = document.getElementById('cat');
+let catName = document.getElementById('catname');
+let counterElement = document.getElementById('counter');
+let listOfCats = [{
+    name: "Miau",
+    website: "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?auto=compress&cs=tinysrgb&h=350"
+}, {
+    name: "Bobby",
+    website: "https://images.pexels.com/photos/126407/pexels-photo-126407.jpeg?auto=compress&cs=tinysrgb&h=350"
+}, {
+    name: "Choppy",
+    website: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350"
+}, {
+    name: "Mokky",
+    website: "https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg"
+}, {
+    name: "Floki",
+    website: "https://cdn.shopify.com/s/files/1/0673/5325/files/LadyDinahs_Cat_Cafe_Alice-Block_2048x.jpg?v=1517059999"
+}]
+
+let currentCatCounter = 0;
+let clickCounter = function () {
+    currentCatCounter++;
+    counterElement.innerText = currentCatCounter;
+    console.log('click');
+}
+
+console.log(listOfCats[0].name);
+let eventAdder = function() {
+    for(let i=0; i<listOfCats.length; i++) {
+            let currentListElement = 'list' + i;
+            let currentCat = document.getElementById(currentListElement);
+            let currentCatName = listOfCats[i].name;
+            let currentCatPicture = listOfCats[i].website;
+            let currentCatCounter = 0;
+            currentCat.addEventListener('click', function(catNameCopy, currentCatCounterCopy) {
+                return function(){
+                catName.innerText = catNameCopy;    
+                catPhoto.src = currentCatPicture;
+                currentCatCounterCopy++;
+                counterElement.innerText = currentCatCounterCopy; 
+                }
+            }(currentCatName, currentCatCounter));
+            
+    }
+};
+
+eventAdder();
